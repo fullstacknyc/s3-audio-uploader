3e9e375a65bba356529961462e46c90ddf271537
fullstacknyc <jgomezval@icloud.com>
2025-04-30 16:59:57 -0400

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,18 @@ export default function RootLayout({
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2330891452848767"
           crossOrigin="anonymous"></script>
         )}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JJMFNDZXLC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JJMFNDZXLC');
+            `,
+          }}
+        />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
